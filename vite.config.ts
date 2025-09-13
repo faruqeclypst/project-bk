8d8a48ebae55c5684d6ab5f44bc0c155c1d8288a
faruqeclypst <faruq.blogger@gmail.com>
2025-09-13 11:34:44 +0700

    fix build error and simplify vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_PB_URL': JSON.stringify(process.env.VITE_PB_URL || 'http://206.189.32.190:8090')
-  }
 })
